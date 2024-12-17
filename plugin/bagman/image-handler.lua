a9cd5b1a95189cb09dba800755e3647459d2c1af
saltkid <ojisaltkid@gmail.com>
2024-12-18 01:11:27 +0800

    feat(image-handler): add more object-fit resizing strategies other than contain
    * cover
       - resize image to fill entire window while keeping aspect ratio
       - allows clipping
    
    * fill
       - resize image to fill entire window without keeping aspect ratio
       - no clipping
       - you can already do this in wezterm by doing "Contain" on both
    height and width but I reimplemented it so it can be consistent with
    other object-fit strategies that rely on pixel numbers
    
    * none
       - keeps original size
    
    * scale-down
       - either does contain or none, whichever one results in the smaller
    image

--- plugin/bagman/image-handler.lua
+++ plugin/bagman/image-handler.lua
@@ -40,29 +40,87 @@ function M.dimensions(image)
 	return width, height, true
 end
 
----Computes the `contain` width and height of an image using current window width and height as
----basis.
+---Returns the resized dimensions of an image based on the specified object_fit strategy
+---@param image_width number
+---@param image_height number
+---@param window_width number
+---@param window_height number
+---@param object_fit "contain" | "cover" | "fill" | "none" | "scale-down"
+---@return number new_width
+---@return number new_height
+function M.resize_image(image_width, image_height, window_width, window_height, object_fit)
+	if "contain" == object_fit then
+		local new_width, new_height = M.contain_dimensions(image_width, image_height, window_width, window_height)
+		return new_width, new_height
+	elseif "cover" == object_fit then
+		local new_width, new_height = M.cover_dimensions(image_width, image_height, window_width, window_height)
+		return new_width, new_height
+	elseif "fill" == object_fit then
+		return window_width, window_height
+	elseif "none" == object_fit then
+		return image_width, image_height
+	elseif "scale-down" == object_fit then
+		local new_width, new_height = M.scale_down_dimensions(image_width, image_height, window_width, window_height)
+		return new_width, new_height
+	else
+		wezterm.log_error("BAGMAN IMAGE HANDLER ERROR: unknown object_fit:", object_fit)
+		return image_width, image_height
+	end
+end
+
+---Computes css's `object-fit: contain` width and height of an image using current window width and
+---height as basis.
 ---Simulates css's `object-fit: contain` where the image keeps its aspect ratio, but is resized to
----fit within the given window dimensions. This is unlike `cover` where the image keeps its aspect
----ratio and fills the given dimension but will be clipped to fit
----@param width number width of image to scale
----@param height number height of image to scale
----@param windowWidth number width of current window
----@param windowHeight number height of current window
+---fit within the given window dimensions.
+---@param image_width number
+---@param image_height number
+---@param window_width number
+---@param window_height number
 ---@return number width
 ---@return number height
-function M.contain_dimensions(width, height, windowWidth, windowHeight)
-	local ratio = width / height
-	local width_val = 0
-	local height_val = 0
-	if ratio > 1 then
-		width_val = windowWidth
-		height_val = math.floor(windowWidth / ratio)
+function M.contain_dimensions(image_width, image_height, window_width, window_height)
+	if image_width > image_height then
+		return window_width, image_height * math.floor(window_width / image_width)
 	else
-		width_val = math.floor(ratio * windowHeight)
-		height_val = windowHeight
+		return image_width * math.floor(window_height / image_height), window_height
 	end
-	return width_val, height_val
+end
+
+---Computes css's `object-fit: cover` width and height of an image using current window width and
+---height as basis.
+---This can already be done in wezterm by doing "Cover" on whichever dimension is smaller and nil
+---on the other. This is just implemented again to stay consisten with other object_fits working
+---with numbers
+---@param image_width number
+---@param image_height number
+---@param window_width number
+---@param window_height number
+---@return number width
+---@return number height
+function M.cover_dimensions(image_width, image_height, window_width, window_height)
+	if image_width > image_height then
+		return image_width * math.floor(window_height / image_height), window_height
+	else
+		return window_width, image_height * math.floor(window_width / image_width)
+	end
+end
+
+---Computes css's `object-fit: scale-down` width and height of an image using current window width
+---and height as basis.
+---This does either `object-fit: none` or `object-fit: contain`, whichever one is smaller.
+---@param image_width number
+---@param image_height number
+---@param window_width number
+---@param window_height number
+---@return number width
+---@return number height
+function M.scale_down_dimensions(image_width, image_height, window_width, window_height)
+	if image_width > window_width or image_height > window_height then
+		local width_val, height_val = M.contain_dimensions(image_width, image_height, window_width, window_height)
+		return width_val, height_val
+	else
+		return image_width, image_height
+	end
 end
 
 return M
