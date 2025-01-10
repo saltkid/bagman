58755db624bbef60d9a74a10cbb4080240943222
saltkid <ojisaltkid@gmail.com>
2025-01-10 11:43:37 +0800

    feat(utils): table.deep_copy()

--- plugin/bagman/utils.lua
+++ plugin/bagman/utils.lua
@@ -1,9 +1,11 @@
+---@class BagmanUtils
 local M = {}
 -- additional table methods
 M.table = {}
 
 -- returns a shallow copy of a table. this means table values will still be
 -- by reference.
+-- used for tables containing atomic types
 ---@param tbl table
 ---@return table tbl_copy
 function M.table.shallow_copy(tbl)
@@ -12,6 +14,29 @@ function M.table.shallow_copy(tbl)
 		tbl_copy[k] = v
 	end
 	return tbl_copy
+end
+
+-- returns a deep copy of a table.
+-- used for when tables contain other tables.
+-- credits: https://gist.github.com/tylerneylon/81333721109155b2d244
+---@param tbl table
+---@param seen? table whether we saw this table before, to avoid inf recursion
+---@return table tbl_copy
+function M.table.deep_copy(tbl, seen)
+	if type(tbl) ~= "table" then
+		return tbl
+	end
+	if seen and seen[tbl] then
+		return seen[tbl]
+	end
+
+	local s = seen or {}
+	local res = {}
+	s[tbl] = res
+	for k, v in pairs(tbl) do
+		res[M.table.deep_copy(k, s)] = M.table.deep_copy(v, s)
+	end
+	return setmetatable(res, getmetatable(tbl))
 end
 
 return M
